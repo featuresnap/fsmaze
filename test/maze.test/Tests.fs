19ae50117ba1446f5f5098383b2e26e47128f48d
featuresnap <pkelly14159@hotmail.com>
2020-05-06 00:38:55 +0000

    grid with duplicate cell links

--- test/maze.test/Tests.fs
+++ test/maze.test/Tests.fs
@@ -72,3 +72,14 @@ let ``links cells in 1 X 2 grid includes reverse link``() =
     |> hasLink (0,1) (0,0)
     |> should be True
 
+[<Fact>]
+let ``links cells in 2 X 2 grid includes reverse link``() =
+ 
+    GridBuilder(2, 2)
+    |> withLink (0,0) (0,1)
+    |> withLink (0,0) (1,0)
+    |> hasLink (0,1) (0,0)
+    |> should be True
+
+
+
