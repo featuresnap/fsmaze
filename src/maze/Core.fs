6152b173fd1d198a4d6755b286df91054d88ad83
featuresnap <pkelly14159@hotmail.com>
2020-05-06 01:20:57 +0000

    refactor: remove unused code

--- src/maze/Core.fs
+++ src/maze/Core.fs
@@ -1,15 +1,12 @@
 ﻿namespace Maze
 
 module Core =
-    open System.Text
     open System.Collections.Generic
     open System.Collections.Concurrent
-
 
     type GridBuilder(rows, cols) =
         let links =
             ConcurrentDictionary<int * int, HashSet<int * int>>()
-
 
         let hashSetFactory = fun _ -> HashSet<int * int>()
 
@@ -20,56 +17,20 @@ module Core =
         member x.ColumnCount = cols
 
         member internal x.AddLink(fromCell, toCell) =
-            links.GetOrAdd(fromCell, hashSetFactory).Add(toCell) |> ignore
-            links.GetOrAdd(toCell, hashSetFactory).Add(fromCell) |> ignore
+            links.GetOrAdd(fromCell, hashSetFactory).Add(toCell)
+            |> ignore
+            links.GetOrAdd(toCell, hashSetFactory).Add(fromCell)
+            |> ignore
 
         member internal x.HasLink(fromCell, toCell) =
             let (fromExists, fromLinks) = links.TryGetValue fromCell
-            let (toExists, toLinks) = links.TryGetValue toCell            
+            let (toExists, toLinks) = links.TryGetValue toCell
 
-            (fromExists &&  fromLinks.Contains(toCell)) ||
-            (toExists && toLinks.Contains(fromCell))
-
-
-    let private appendChar (c: char) (sb: StringBuilder) = sb.Append(c) |> ignore
-    let private appendStr (s: string) (sb: StringBuilder) = sb.Append(s) |> ignore
-
-    let display (gridBuilder: GridBuilder): string list =
-
-        let maxRow = gridBuilder.RowCount - 1
-        let maxCol = gridBuilder.ColumnCount - 1
-
-        let tops = "+---"
-        let middles = "|   "
-
-        let makeRow rowIndex =
-            [ let sbTop = StringBuilder()
-              let sbMiddle = StringBuilder()
-
-              for col in 0 .. maxCol do
-                  sbTop |> appendStr tops
-                  sbMiddle |> appendStr middles
-
-              sbTop |> appendChar '+'
-              sbMiddle |> appendChar '|'
-
-              yield sbTop |> string
-              yield sbMiddle |> string ]
-
-        let makeBottomEdge maxRow =
-            let sb = StringBuilder()
-            for col in 0 .. maxCol do
-                sb |> appendStr "+---"
-            sb.Append('+') |> string
-
-        [ if maxRow >= 0 && maxCol >= 0 then
-            for rowIndex in 0 .. (gridBuilder.RowCount - 1) do
-                yield! makeRow rowIndex
-            yield makeBottomEdge maxRow ]
+            (fromExists && fromLinks.Contains(toCell))
+            || (toExists && toLinks.Contains(fromCell))
 
     let withLink cellFrom cellTo (gridBuilder: GridBuilder): GridBuilder =
         gridBuilder.AddLink(cellFrom, cellTo)
         gridBuilder
-
 
     let hasLink cellFrom cellTo (gridBuider: GridBuilder): bool = gridBuider.HasLink(cellFrom, cellTo)
