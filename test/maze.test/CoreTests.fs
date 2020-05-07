54273ce39c58f9a855b7c1b44edad5cd9366e31c
featuresnap <pkelly14159@hotmail.com>
2020-05-07 01:30:02 +0000

    test exit functions

--- test/maze.test/CoreTests.fs
+++ test/maze.test/CoreTests.fs
@@ -30,3 +30,40 @@ module CoreTests =
         |> withLink (0,0) (1,0)
         |> hasLink (0,1) (0,0)
         |> should be True
+
+    [<Fact>]
+    let ``add left exit at top left corner``() =
+     
+        GridBuilder(2, 2)
+        |> withExit (0,0) Left
+        |> hasLink (0,0) (0,-1)
+        |> should be True
+
+    [<Fact>]
+    let ``add top exit at top left corner``() =
+     
+        GridBuilder(2, 2)
+        |> withExit (0,0) Top
+        |> hasLink (0,0) (-1,0)
+        |> should be True
+
+    [<Fact>]
+    let ``add right exit at bottom right corner``() =
+     
+        GridBuilder(2, 2)
+        |> withExit (1,1) Right
+        |> hasLink (1,1) (1,2)
+        |> should be True
+
+    [<Fact>]
+    let ``add bottom exit at bottom right corner``() =
+     
+        GridBuilder(2, 2)
+        |> withExit (1,1) Bottom
+        |> hasLink (1,1) (2,1)
+        |> should be True
+
+
+
+
+
