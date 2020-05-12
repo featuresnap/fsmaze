b19d310a21a03a81ff4201ab1a57791b5e76ea3b
featuresnap <pkelly14159@hotmail.com>
2020-05-12 01:04:32 +0000

    display maze with no passages

--- src/maze.console/Program.fs
+++ src/maze.console/Program.fs
@@ -2,10 +2,16 @@
 
 open System
 open Maze.Core
+open Maze.TextDisplay
 open System.CommandLine
 open System.CommandLine.Invocation
 
 module Program = 
+
+    let generateMaze rows cols = 
+       GridBuilder(rows, cols)
+        
+
     [<EntryPoint>]
     let main argv =
         let rootCommand = RootCommand()
@@ -22,7 +28,10 @@ module Program =
         rootCommand.Handler <- CommandHandler.Create<int, int>(
             fun rows cols -> 
                 printfn "Hello World from F#!"
-                printfn "rows %d, columns %d" rows cols)
+                printfn "rows %d, columns %d" rows cols
+                generateMaze rows cols 
+                |> display 
+                |> Seq.iter (printfn "%s"))
         rootCommand.Invoke(argv) |> ignore
         printfn "Hello World from outside command"
 
