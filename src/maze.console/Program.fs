1af4eefcfcf76278d22446b1e5d857466ef8f62c
featuresnap <pkelly14159@hotmail.com>
2020-05-12 00:50:49 +0000

    feat: add Program module, using System.CommandLine library

--- src/maze.console/Program.fs
+++ src/maze.console/Program.fs
@@ -1,8 +1,29 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿namespace Maze.Console
 
 open System
+open Maze.Core
+open System.CommandLine
+open System.CommandLine.Invocation
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+module Program = 
+    [<EntryPoint>]
+    let main argv =
+        let rootCommand = RootCommand()
+        rootCommand.Add (
+            Option<int>(
+                [|"--rows"; "-r"|], 
+                getDefaultValue = (fun () -> 3), 
+                description = "number of rows"))
+        rootCommand.Add (
+            Option<int>(
+                [|"--cols"; "-c"|], 
+                getDefaultValue = (fun () -> 3), 
+                description = "number of columns"))
+        rootCommand.Handler <- CommandHandler.Create<int, int>(
+            fun rows cols -> 
+                printfn "Hello World from F#!"
+                printfn "rows %d, columns %d" rows cols)
+        rootCommand.Invoke(argv) |> ignore
+        printfn "Hello World from outside command"
+
+        0 // return an integer exit code
